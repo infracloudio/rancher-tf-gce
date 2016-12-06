24c2e42c4d1bd5112bd9e062f278912548cdfc40
Harshal Shah <harshal@infracloud.io>
2016-12-06 17:16:18 +0530

    Fixing APIV2 issues of rancher

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,7 +28,6 @@ else
 #
 # Agent ADD command here, capture Master IP in $2
 #
-#sudo docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.0.2 http://23.251.153.192:8080/v1/scripts/4A1E49D498DE4B51BD9F:1478023200000:6GEV95neEurg6qy6OvBNbe47g8
-chmod 755 /tmp/installAgent.sh && /tmp/installAgent.sh $2 1a7
+chmod 755 /tmp/installAgent.sh && /tmp/installAgent.sh $2
 echo "pip install etc here"
 fi
